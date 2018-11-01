666de374f3f33c5a3c0174fc74f9706741471469
Michael Nelson <revnelson@gmail.com>
2018-11-01 11:19:13 +0800

    Updates

---
+++ precache-manifest.0bc8146d4de2c2086333f663a1354202.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "1edc0d064949ce4f68db",
+    "url": "/robofriends/static/js/main.1edc0d06.chunk.js"
+  },
+  {
+    "revision": "1f44d6f418a6566cef65",
+    "url": "/robofriends/static/js/1.1f44d6f4.chunk.js"
+  },
+  {
+    "revision": "1edc0d064949ce4f68db",
+    "url": "/robofriends/static/css/main.cc0c6985.chunk.css"
+  },
+  {
+    "revision": "1f44d6f418a6566cef65",
+    "url": "/robofriends/static/css/1.7240567f.chunk.css"
+  },
+  {
+    "revision": "800153b2ff40c3d0646c5d44c0eeab0d",
+    "url": "/robofriends/index.html"
+  }
+];
