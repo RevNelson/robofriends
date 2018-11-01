fa4680cdbd1a99623e5291fa67e944d67fd06f37
Michael Nelson <revnelson@gmail.com>
2018-11-01 12:28:15 +0800

    Improvements for lighthouse scores: favicons, input labels metadata

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -4,13 +4,13 @@ const SearchBox = ({searchField, searchChange }) => {
     return (
         <div className='pa2'>
             <input
+                aria-label='Search Robots'
                 className='pa3 ba b--green bg-lightest-blue'
                 type='search'
                 id='searchfield'
                 placeholder='search robots'
                 onChange={searchChange}
             />
-            <label className='dn' for='searchfield'>Search Robots</label>
         </div>
     );
 };
